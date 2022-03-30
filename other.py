2109a0ec59388dd1a2c0503b83231e7432de941c
JULIAN_RIOS <jdrios111@gmail.com>
2022-03-30 12:42:26 -0500

    first example

--- other.py
+++ other.py
@@ -1,2 +1,12 @@
+#!/usr/bin/env python3
+def dif(func):
+    def inner(a,b):
+        print("Let's make a ",func.__name__)
+        return func(a,b)
+    return inner
+
+@dif
 def sum(a,b):
     return a+b
+
+print(sum(1,2))
